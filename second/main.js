8b90f72da6107c88ed8f0b61e2c3eb4e531ef2ac
lacctrDress20 <1490244@senati.pe>
2023-04-23 16:16:30 -0500

    testimonios part final

--- second/main.js
+++ second/main.js
@@ -10,6 +10,7 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
