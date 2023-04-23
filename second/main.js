8f14f0fc095e4d675c832b7b93b5c053150b8024
lacctrDress20 <1490244@senati.pe>
2023-04-23 16:03:22 -0500

    testimonios part 1.6

--- second/main.js
+++ second/main.js
@@ -3,3 +3,19 @@ let header = document.querySelector('.header');
 
 header.classList.toggle('Sticky', window.scrollY > 100);
 };
+
+
+/*=========== swiper ==========*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  })
